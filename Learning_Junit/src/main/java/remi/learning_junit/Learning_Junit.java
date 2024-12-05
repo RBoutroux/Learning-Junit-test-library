2ea414547d8bc93a9e1b9b4469314cd1ac696554
RBoutroux <remibtrx@gmail.com>
2024-12-05 08:26:49 +0100

    [feat] Documents TP

---
+++ Learning_Junit/src/main/java/remi/learning_junit/Learning_Junit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package remi.learning_junit;
+
+/**
+ *
+ * @author remib
+ */
+public class Learning_Junit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
